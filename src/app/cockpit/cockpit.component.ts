743fc3bdd6b2e90ca66b4ea81444a513472c39ca
R. Subash <subashr9803@gmail.com>
2020-04-30 10:42:38 +0530

    First Angular Project

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+@Output()  serverCreated = new EventEmitter<{serverName:string, serverContent:string}>();
+@Output('bpCreated')  bluePrintCreated = new EventEmitter<{serverName:string, serverContent:string}>();;
+ //newServerName = '';
+ // newServerContent = '';
+ @ViewChild('serverContentInput',{static:true}) serverContentInput: ElementRef;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+   onAddServer(nameInput:HTMLInputElement) {
+  // console.log(this.serverContentInput);
+   this.serverCreated.emit({serverName:nameInput.value, serverContent:this.serverContentInput.nativeElement.value});
+  }
+
+  onAddBlueprint(nameInput: HTMLInputElement) {
+  //this.serverContentInput.nativeElement.value='something';
+     this.bluePrintCreated.emit({serverName:nameInput.value, serverContent:this.serverContentInput.nativeElement.value});
+  }
+
+}
---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, ViewChild, ElementRef , ContentChild} from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class ServerElementComponent implements OnInit, OnChanges,DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+@Input('srvelement')	element:{type:string, name:string, content:string};
+@ViewChild('heading',{static:true}) header: ElementRef
+@ContentChild('contentParagraph',{static:true}) paragraph: ElementRef;
+  constructor() { 
+  console.log("Constructor get called");
+  }
+  ngOnChanges(changes: SimpleChanges){
+  console.log("ngOnChange called");
+  console.log(changes);
+
+  }
+
+  ngOnInit(): void {
+  console.log("ngOnInit called");
+  console.log("Text Content" +this.header.nativeElement.textContent);
+  console.log("Text content of para" + this.paragraph.nativeElement.textContent);
+  }
+  ngDoCheck(){
+  console.log("ngDoCheck called");
+  }
+  ngAfterContentInit()
+  {
+  console.log("ngAfterContentInit called");
+    console.log("Text content of para" + this.paragraph.nativeElement.textContent);
+  }
+  ngAfterContentChecked()
+  {
+  console.log("ngAfterContentchecked called");
+  }
+  ngAfterViewInit()
+  {
+  console.log("ngAfterViewInit callled");
+  console.log("Text Content" +this.header.nativeElement.textContent)
+  }
+  ngAfterViewChecked()
+  {
+  console.log("ngAfterViewChecked called");
+  }
+  ngOnDestroy()
+  {
+  console.log("ngOnDestroy called");
+  }
+
+}
